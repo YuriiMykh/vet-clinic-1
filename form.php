671784809c1fddc787cef5606ab87d1620113758
Yurii Mykh <yurikanunak@gmail.com>
2019-04-07 16:52:08 +0000

    addade about section

---
+++ form.php
@@ -0,0 +1,7 @@
+<p>
+    Welcome <?php echo $_POST["name"]; ?><br>
+    Topic is: <?php echo $_POST["topic"]; ?><br>
+    Email is: <?php echo $_POST["e-mail"]; ?> <br>
+    Phone number is: <?php echo $_POST["client-phone"]; ?> <br>
+    Your message is: <?php echo $_POST["message"]; ?>
+</p>
---
+++ js/index.js
@@ -0,0 +1,20 @@
+
+
+document.querySelector('.doctors-consultation .send-form').addEventListener('submit', submitForm);
+const textarea = document.querySelector('textarea[name="message"]');
+
+function submitForm(e) {
+    //textarea.value = " ";
+    e.preventDefault();
+    fetch('form.php', {
+        method: 'POST',
+        body: new FormData(document.querySelector('.doctors-consultation .send-form'))
+    })
+    .then( response => response.text())
+    .then( html => {   
+        textarea.value = "";
+        textarea.value = html.slice(3, html.length-5).split('<br>') 
+    });
+    
+}
+
