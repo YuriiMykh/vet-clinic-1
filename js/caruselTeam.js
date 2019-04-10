b2caafe56d899c9f25f2ebdbab5d92d4eaaa5917
Yurii Mykh <yurikanunak@gmail.com>
2019-04-10 14:10:22 +0000

    bugfixing2

--- js/caruselTeam.js
+++ js/caruselTeam.js
@@ -1,9 +1,9 @@
 
 //one doctor
-const doctor1 = document.querySelector('main .team .personel .small-pic-personel .doctor1');
-console.log(doctor1);
-doctor1.addEventListener('click', doctorPicPressed);
-//4 doctors
+// const doctor1 = document.querySelector('main .team .personel .small-pic-personel .doctor1');
+// console.log(doctor1);
+// doctor1.addEventListener('click', doctorPicPressed);
+// //4 doctors
 const doctors = document.querySelectorAll('main .team .personel .small-pic-personel>div');
 doctors.forEach(doctor => doctor.addEventListener('click', doctorPicPressed));
 const arrayDoctors = Array.from(doctors);
@@ -12,14 +12,24 @@ console.log(doctors);
 function doctorPicPressed(e){
     e.preventDefault();
     
-    console.log( e.target.className);
-//   console.log(('c;ocked '+ e.target.className + ' clicked'));
-    // switch(e.target.className){
+    console.log( e.target.parentNode.className);
+   
+    switch(e.target.parentNode.className){
         
-    //     case 'doctor1': {
-    //         alert(e.target.className + 'clicked')
-    //         break;}
+        case 'doctor1': {
+            //e.target.parentNode.style.border-color = " #fff";
+            e.target.parentNode.style.borderColor = '#000000';
+            break;}
+        case 'doctor2': {
+            //alert(e.target.parentNode.className + ' clicked')
+            break;}
             
-    //         // style.border-color = " #fff";
-    // }
+        case 'doctor3': {
+            //alert(e.target.parentNode.className + ' clicked')
+            break;}
+        case 'doctor4': {
+            //alert(e.target.parentNode.className + ' clicked')
+            break;}    
+            // style.border-color = " #fff";
+    }
 }
